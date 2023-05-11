9072b34b34b3e44ce1ee1fae3672126d140b6c66
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-05-11 14:00:12 +0900

    Create docker-install.sh
    ubuntu 22.04

---
+++ docker-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+# configure daemon.json
+sudo mkdir -p /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "100m"
+    },
+    "storage-driver": "overlay2"
+}
+EOF
+
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
