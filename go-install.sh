37c2437d007e03673608eb4ba839c0c82847aa45
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-08-08 19:48:54 +0900

    Update go-install.sh

--- go-install.sh
+++ go-install.sh
@@ -10,7 +10,7 @@ fi
 cd $HOME
 curl -LO https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
-cat ~/.profile | grep /usr/local/go/bin || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profil
+cat ~/.profile | grep /usr/local/go/bin || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 cat ~/.profile | grep GOPATH= || echo 'export GOPATH=$(go env GOPATH)' >> ~/.profile
 cat ~/.profile | grep GOPATH/bin || echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.profile
 source ~/.profile
