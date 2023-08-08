f7a84971ad40e473dadbe304dce7af5ee8d5757f
Kyumin Kim <68532437+1006lem@users.noreply.github.com>
2023-08-08 19:43:20 +0900

    Create go-install.sh

---
+++ go-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check operating system
+if [[ $(uname -s) != "Linux" ]]; then
+    echo "This script is for Linux."
+    exit 1
+fi
+
+
+cd $HOME
+curl -LO https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+cat ~/.profile | grep /usr/local/go/bin || echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profil
+cat ~/.profile | grep GOPATH= || echo 'export GOPATH=$(go env GOPATH)' >> ~/.profile
+cat ~/.profile | grep GOPATH/bin || echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.profile
+source ~/.profile
+
+echo $GOPATH   # home/kmkim/go
+go version     # go version go1.20.5 linux/amd64
+
